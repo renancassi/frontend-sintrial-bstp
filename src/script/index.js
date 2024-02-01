3ed46f3ac2b1cf767882cf03ffabfc9c89823314
Renan Cassi <48595316+renancassi@users.noreply.github.com>
2024-02-01 07:52:57 -0300

    Novas telas Novos formulários Tela de erro Correção de alguns bugs

--- src/script/index.js
+++ src/script/index.js
@@ -53,12 +53,53 @@ document.addEventListener('DOMContentLoaded', function () {
 
 
 
+  document.addEventListener('DOMContentLoaded', function () {
+    const modal = new bootstrap.Modal(document.getElementById('faleConoscoModal'));
+
+      function openModal(title, imageSrc, content, telefone, endereco) {
+      const modalTitle = document.getElementById('faleConoscoModalLabel');
+      const modalImage = document.getElementById('modalImage');
+      const modalContent = document.getElementById('modalContent');
+      const modalTelefone = document.getElementById('modalTelefone');
+      const modalEndereco = document.getElementById('modalEndereco');
+
+      modalTitle.textContent = title;
+      modalImage.src = imageSrc;
+      modalContent.innerHTML = content;
+      modalTelefone.textContent = 'Contato: ' + telefone;
+      modalEndereco.textContent = 'Endereço: ' + endereco;
+
+      modal.show();
+    }
+
+    const continueButtons = document.querySelectorAll('.faleConosco .btn');
+    continueButtons.forEach(button => {
+      button.addEventListener('click', function () {
+        const card = this.closest('.card');
+        const cardTitle = card.querySelector('.faleConoscoCardTitulo').textContent;
+        const cardImageSrc = card.querySelector('.card-img-top').src;
+        const cardContent = card.querySelector('.limiteTexto').innerHTML;
+        const cardTelefone = card.getAttribute('data-telefone'); 
+        const cardEndereco = card.getAttribute('data-endereco');
+        const cardFullText = card.querySelector('.limiteTexto').getAttribute('data-fullText'); 
+
+        openModal(cardTitle, cardImageSrc, cardFullText, cardTelefone, cardEndereco);
+      });
+    });
+  });
+
+
+
+//EMPRESA whatsapp
+//EMPRESA whatsapp
+//EMPRESA whatsapp
+
   const setup = (function() {
     // console.log('setup');
     document.getElementById('sendMessage').onsubmit = function(e) {
         e.preventDefault();
         console.log('click');
-        const meuNumero = '554688139272';
+        const meuNumero = '46999354025';
         const nome_responsavel = (document.getElementById('nome_responsavel').value)
         const cargo = (document.getElementById('cargo_empresa').value)
         const empresa = (document.getElementById('nome_empresa').value)
@@ -70,7 +111,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
         const text = 'Olá, desejo cadastrar minha empresa. Meu nome é *' + nome_responsavel + '* - Email: *' + email + '* - CNPJ: *' + cnpj + '* Empresa: *' + empresa + '* Cargo: *' + cargo + '*';
         if (!number) {
-            alert('Invalid number');
+            alert('Digite um número de telefone válido!');
             return;
         }
         // const link = https://wa.me/${country}${number}?text=${text};
@@ -80,8 +121,46 @@ document.addEventListener('DOMContentLoaded', function () {
         window.open(link);
     };
 })
+
 if (window.attachEvent) {
-    window.attachEvent('onload', setup);
+  window.attachEvent('onload', setup);
 } else {
-    window.addEventListener('load', setup, false);
+  window.addEventListener('load', setup, false);
+}
+
+//SÓCIO whatsapp
+//SÓCIO whatsapp
+//SÓCIO whatsapp
+
+const setupSocio = (function() {
+  // console.log('setup');
+  document.getElementById('sendMessageSocio').onsubmit = function(e) {
+      e.preventDefault();
+      console.log('click');
+      const meuNumero = '46999354025';
+      const nome_socio = (document.getElementById('nome_socio').value)
+      const cargo = (document.getElementById('cargo_empresa').value)
+      const empresa = (document.getElementById('nome_empresa').value)
+      const email = (document.getElementById('email_socio').value)
+      const number = (document.getElementById('telefone_socio').value || '')
+          .replace(/\D/g, '')
+      // .replace(new RegExp(^${country}), '');
+
+      const text = 'Olá, desejo ser sócio. Meu nome é *' + nome_socio + '* - Email: *' + email + '* Empresa: *' + empresa + '* Cargo: *' + cargo + '*';
+      if (!number) {
+          alert('Digite um número de telefone válido!');
+          return;
+      }
+      // const link = https://wa.me/${country}${number}?text=${text};
+      link = `https://web.whatsapp.com/send?phone=${meuNumero}&text=${text}`;
+
+      console.log(number, link);
+      window.open(link);
+  };
+})
+
+if (window.attachEvent) {
+  window.attachEvent('onload', setupSocio);
+} else {
+  window.addEventListener('load', setupSocio, false);
 }
